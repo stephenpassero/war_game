5e839c7800e9c19f962847602968747d06040dc6
Stephen Passero <stephen.passero@gmail.com>
2018-05-29 15:47:23 -0400

    Refactor code to use attr methods

--- lib/card.rb
+++ lib/card.rb
@@ -1,14 +1,7 @@
 class Card
+  attr_reader(:suit, :value)
   def initialize(card_suit, card_value)
     @suit = card_suit
     @value = card_value
-  end
-
-  def suit()
-    @suit
-  end
-
-  def value()
-    @value
   end
 end
