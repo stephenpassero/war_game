da592ec4a05e8512095f8ba7b5e9fae3938394bc
Stephen Passero <stephen.passero@gmail.com>
2018-05-30 11:22:44 -0400

    Make some tweaks

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,12 +2,6 @@ require("rspec")
 require("game")
 
 describe("#game") do
-  # it("should be able to create a new deck") do
-  #   game = Game.new()
-  #   deck = game.create_new_deck()
-  #   expect(deck.cards_left).to eq(52)
-  # end
-
   it("should be able to split up the deck between both players") do
     game = Game.new()
     player1 = Player.new()
