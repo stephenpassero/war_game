b50e3c35c14b6cbcaba038986afdbd94917defc7
Stephen Passero <stephen.passero@gmail.com>
2018-05-30 12:24:53 -0400

    Implement ability to handle ties

--- lib/game.rb
+++ lib/game.rb
@@ -38,6 +38,26 @@ class Game
     elsif card2_value > card1_value
       @player2.deck.add([card1, card2])
       return card2
+    elsif card1_value == card2_value
+      collection_of_cards = [card1, card2]
+      counter = 0
+      until counter >= 3
+        collection_of_cards.push(@player1.play_top_card(), @player2.play_top_card())
+        counter += 1
+      end
+      new_card_1 = @player1.play_top_card()
+      new_card_2 = @player2.play_top_card()
+      collection_of_cards.push(new_card_1, new_card_2)
+      new_card1_value = @cardValues.fetch(new_card_1.rank)
+      new_card2_value = @cardValues.fetch(new_card_2.rank)
+      if new_card1_value > new_card2_value
+        @player1.deck.add(collection_of_cards)
+        return new_card_1
+      elsif new_card2_value > new_card1_value
+        @player2.deck.add(collection_of_cards)
+        return new_card_2
+      end
     end
   end
+
 end
