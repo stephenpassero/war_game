50ec45fe3285002d778f65ab18425807ef302133
Stephen Passero <stephen.passero@gmail.com>
2018-05-31 08:59:38 -0400

    Tweak Game's judge method to handle doulbe wars and create the new war_runner file

--- lib/game.rb
+++ lib/game.rb
@@ -34,10 +34,10 @@ class Game
     card2_value = @cardValues.fetch(card2.rank)
     if card1_value > card2_value
       @player1.deck.add([card1, card2])
-      return card1
+      return "Player 1 took a #{card2.rank} of #{card2.suit} with a #{card1.rank} of #{card1.suit}"
     elsif card2_value > card1_value
       @player2.deck.add([card1, card2])
-      return card2
+      return "Player 2 took a #{card1.rank} of #{card1.suit} with a #{card2.rank} of #{card2.suit}"
     elsif card1_value == card2_value
       collection_of_cards = [card1, card2]
       counter = 0
@@ -52,10 +52,12 @@ class Game
       new_card2_value = @cardValues.fetch(new_card_2.rank)
       if new_card1_value > new_card2_value
         @player1.deck.add(collection_of_cards)
-        return new_card_1
+        return "War! Player 1 took a #{new_card_2.rank} of #{new_card_2.suit} with a #{new_card_1.rank} of #{new_card_1.suit}"
       elsif new_card2_value > new_card1_value
         @player2.deck.add(collection_of_cards)
-        return new_card_2
+        return "War! Player 2 took a #{new_card_1.rank} of #{new_card_1.suit} with a #{new_card_2.rank} of #{new_card_2.suit}"
+      elsif new_card_1_value == new_card_2new_card2_value
+        judge(new_card_1, new_card_2)
       end
     end
   end
